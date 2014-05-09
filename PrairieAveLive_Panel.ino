154b5573fe14e895b4c69f82665f2bbe3b10e83f
balbano <balbano@gmail.com>
2014-05-09 14:12:24 -0500

    Code cleanup.

--- PrairieAveLive_Panel.ino
+++ PrairieAveLive_Panel.ino
@@ -1,5 +1,5 @@
 /*  
-  Required Connections
+  Required Connections (From OCTOWS2811 example)
   --------------------
     pin 2:  LED Strip #1    OctoWS2811 drives 8 LED Strips.
     pin 14: LED strip #2    All 8 are the same length.
@@ -13,43 +13,54 @@
     pin 4 - Do not use
     pin 3 - Do not use as PWM.  Normal use is ok.
 
-  This test is useful for checking if your LED strips work, and which
-  color config (WS2811_RGB, WS2811_GRB, etc) they require.
+  NOTES
+  -----
+  Assumes a 4-byte int, such as that on the Teensy3.1
 */
 
 #include <OctoWS2811.h>
 
+// Panel setup.
 #define LEDS_PER_STRIP 11
 #define STRIPS_PER_PANEL 7
 
-long panel[STRIPS_PER_PANEL][LEDS_PER_STRIP];
-long panelBuffer[STRIPS_PER_PANEL][LEDS_PER_STRIP];
+int panel[STRIPS_PER_PANEL][LEDS_PER_STRIP];
+int panelBuffer[STRIPS_PER_PANEL][LEDS_PER_STRIP];
 
+// Framerate setup
+const float FPS = 2;
+const float frameLength = 1000 / FPS;
+unsigned long previousFrameTime;
+
+// OCTOWS2811 setup
 DMAMEM int displayMemory[LEDS_PER_STRIP*6];
 int drawingMemory[LEDS_PER_STRIP*6];
 
 const int config = WS2811_GRB | WS2811_800kHz;
-
 OctoWS2811 leds(LEDS_PER_STRIP, displayMemory, drawingMemory, config);
 
 void setup() {
   leds.begin();
   leds.show();
+  randomSeed(analogRead(0));
   initializeWithRandomColors(panel);
+  previousFrameTime = millis();
 }
 
 void loop() {
-  iterateGameOfLife(panel, panelBuffer);
-  setPixelsAndShow(panel);
-  delay(500);
+  if ((millis() - previousFrameTime) > frameLength){
+    previousFrameTime = millis();
+    iterateGameOfLife(panel, panelBuffer);
+    setPixelsAndShow(panel);
+  }
 }
 
-void initializeWithRandomColors(long cells[STRIPS_PER_PANEL][LEDS_PER_STRIP])
+void initializeWithRandomColors(int cells[STRIPS_PER_PANEL][LEDS_PER_STRIP])
 {
   for (int y = 0; y < STRIPS_PER_PANEL; y++){
     for(int x = 0; x < LEDS_PER_STRIP; x++){
       if(random(2)){
-        long randomColor = random(0xFFFFFF);
+        int randomColor = random(0xFFFFFF);
         cells[y][x] = randomColor;
       }
       else {
@@ -59,61 +70,64 @@ void initializeWithRandomColors(long cells[STRIPS_PER_PANEL][LEDS_PER_STRIP])
   }
 }
 
-void iterateGameOfLife(long cells[STRIPS_PER_PANEL][LEDS_PER_STRIP], long cellsBuffer[STRIPS_PER_PANEL][LEDS_PER_STRIP])
+void iterateGameOfLife(int cells[STRIPS_PER_PANEL][LEDS_PER_STRIP], int cellsBuffer[STRIPS_PER_PANEL][LEDS_PER_STRIP])
 {
   // Load the current array of cells into the buffer.
-  for (int y = 0; y < STRIPS_PER_PANEL; y++){
-    for(int x = 0; x < LEDS_PER_STRIP; x++){
-      cellsBuffer[y][x] = cells[y][x];
-    }
-  }
+  memcpy(&cellsBuffer, &cells, sizeof(cells));
   
   // Breed and kill the cells
   for (int y = 0; y < STRIPS_PER_PANEL; y++){
     for(int x = 0; x < LEDS_PER_STRIP; x++){
       int neighbors = 0;
-      long neighbors_red_sum = 0;
-      long neighbors_green_sum = 0;
-      long neighbors_blue_sum = 0;
-      // Count the neighbors
+      int neighborsRedSum = 0;
+      int neighborsGreenSum = 0;
+      int neighborsBlueSum = 0;
+      
+      // Iterate through the adjacent cells, count the living neighbors, and sum their r, g and b values.
       for (int yy = y-1; yy <= y+1; yy++) {
         for (int xx = x-1; xx <= x+1; xx++) {
-          if ( ((xx >= 0)&&(xx < LEDS_PER_STRIP)) && ((yy >= 0)&&(yy < STRIPS_PER_PANEL)) ) {
-            if (!((xx == x) && (yy == y))) {
-              if (cellsBuffer[yy][xx]) {
+          if (isValidNeighbor(x, y, xx, yy) && (cellsBuffer[yy][xx] != 0)) { // Live cells have a non-zero color.
                 neighbors++;
-                // Extract the individual colors from the 3-byte hex color.
-                int neighbor_red = (cellsBuffer[yy][xx] >> 16) & 0xFF;
-                int neighbor_green = (cellsBuffer[yy][xx] >> 8) & 0xFF;
-                int neighbor_blue = cellsBuffer[yy][xx] & 0xFF;
-                neighbors_red_sum += neighbor_red;
-                neighbors_green_sum += neighbor_green;
-                neighbors_blue_sum += neighbor_blue;
+                neighborsRedSum += hexToRGB(cellsBuffer[yy][xx], 'r');
+                neighborsGreenSum += hexToRGB(cellsBuffer[yy][xx], 'g');
+                neighborsBlueSum += hexToRGB(cellsBuffer[yy][xx], 'b');
               }
             }
           }
-        }
-      }
-     // Birth and death!
+      
+      // Kill underpopulated and overcrowded cells.
       if (cellsBuffer[y][x]) {
         if ((neighbors < 2) || (neighbors > 3)) {
           cells[y][x] = 0;
         }
       }
+      // Birth cells with 3 neighbors. Make them the average of their neighbors colors.
       else {
         if (neighbors == 3) {
-          int red = neighbors_red_sum / neighbors;
-          int green = neighbors_green_sum / neighbors;
-          int blue = neighbors_blue_sum / neighbors;
-          long color = (red << 16) + (green << 8) + blue;
-          cells[y][x] = color;
+          int avgRed = neighborsRedSum / neighbors;
+          int avgGreen = neighborsGreenSum / neighbors;
+          int avgBlue = neighborsBlueSum / neighbors;
+          cells[y][x] = RGBToHex(avgRed, avgGreen, avgBlue);
         }
-      } 
+      }
+      
     }
   }  
 }
 
-void setPixelsAndShow(long cells[STRIPS_PER_PANEL][LEDS_PER_STRIP])
+boolean isValidNeighbor(int x, int y, int xx, int yy)
+{
+  if ( ( ((xx >= 0)&&(xx < LEDS_PER_STRIP)) && ((yy >= 0)&&(yy < STRIPS_PER_PANEL)) ) // Ignore cells beyond edges.
+     && (!((xx == x) && (yy == y))) )// Cells are not their own neighbors.
+  {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+void setPixelsAndShow(int cells[STRIPS_PER_PANEL][LEDS_PER_STRIP])
 {
   for (int y = 0; y < STRIPS_PER_PANEL; y++){
     for(int x = 0; x < LEDS_PER_STRIP; x++){
@@ -121,4 +135,21 @@ void setPixelsAndShow(long cells[STRIPS_PER_PANEL][LEDS_PER_STRIP])
     }
   }
   leds.show();
+}
+
+int RGBToHex(int red, int green, int blue)
+{
+  return (red << 16) + (green << 8) + blue;
+}
+
+int hexToRGB(int hexColor, char colorToReturn)
+{
+  switch (colorToReturn) {
+    case 'r':
+      return hexColor >> 16;
+    case 'g':
+      return (hexColor >> 8) & 0xFF;
+    case 'b':
+      return hexColor & 0xFF;
+  }
 }
